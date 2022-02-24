2a7ad45876d22da9ebdf7c43984b26ddab3a4100
kylagwenp <kylagwenp@gmail.com>
2022-02-24 21:20:26 +0800

    update

--- greed/__main__.py
+++ greed/__main__.py
@@ -13,6 +13,16 @@ from greed.services.video_service import VideoService
 from greed.shared.color import Color
 from greed.shared.point import Point
 
+FRAME_RATE = 12
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+CAPTION = "Greed"
+WHITE = Color(255, 255, 255)
+DEFAULT_ARTIFACTS = 40
 
 star = ""
 rock = ""
@@ -20,6 +30,18 @@ rock = ""
 def main():
     """Application entry point
     """
+
+    # create the cast
+    cast = Cast()
+
+    # create the banner
+    banner = Actor()
+    banner.set_text("")
+    banner.set_font_size(FONT_SIZE)
+    banner.set_color(WHITE)
+    banner.set_position(Point(CELL_SIZE, 0))
+    cast.add_actor("banners", banner)
+    
     pass
 
 if __name__ == "__main__":
